956a088092fab5c443ebe78cd41f15f8e23fa801
metachemist <meta.chemist.22@gmail.com>
2024-03-01 19:04:35 +0500

    updating git ignore

--- src/BasicsOfJava/GuessingGame.java
+++ src/BasicsOfJava/GuessingGame.java
@@ -1,7 +1,16 @@
 package BasicsOfJava;
 
-public class GuessingGame {
-    public static void main(String[] args){
+import java.util.*;
 
+public class GuessingGame {
+    public static void main(String[] args) {
+        // do{
+        System.out.print("\nHi there amigos, let's get started!\nRule of Game You only should enter number between 1-100 \nEnter your guessed number: ");
+        int numInputted;
+        Scanner scan = new Scanner(System.in);
+        numInputted = scan.nextInt();
+        System.out.println(numInputted);
+        // }while();
+        scan.close();
     }
 }
