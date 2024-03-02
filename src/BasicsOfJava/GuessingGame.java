bcc7bbea106c6bdddc89b93e6392e3f17552ffe8
metachemist <meta.chemist.22@gmail.com>
2024-03-02 08:30:33 +0500

    Adding if else condition

--- src/BasicsOfJava/GuessingGame.java
+++ src/BasicsOfJava/GuessingGame.java
@@ -4,13 +4,23 @@ import java.util.*;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        // do{
+        int numSelected = 65;
         System.out.print("\nHi there amigos, let's get started!\nRule of Game You only should enter number between 1-100 \nEnter your guessed number: ");
-        int numInputted;
         Scanner scan = new Scanner(System.in);
-        numInputted = scan.nextInt();
-        System.out.println(numInputted);
-        // }while();
-        scan.close();
+        int numInputted = scan.nextInt();
+//        System.out.println(numInputted);
+        if(numInputted>0 && numInputted<100){
+            if(numInputted == numSelected){
+                System.out.println("voilà! You guessed correct number");
+            } else if (numInputted < numSelected) {
+                System.out.println("Ouch! your guessed number is less than the actual selected number");
+            }else {
+                System.out.println("Oops, you guessed a greater number than the selected one");
+            }
+        }else{
+            System.out.println("your entered number is against the game rule.");
+        }
+
+
     }
 }
