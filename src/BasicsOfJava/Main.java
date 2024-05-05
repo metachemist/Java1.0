92dd22943da8f081a6cc7a4f37645f05c7d71d33
metachemist <meta.chemist.22@gmail.com>
2024-05-05 07:41:48 +0500

    Java Tutorial Practice

---
+++ src/BasicsOfJava/Main.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+
+public class Main {
+    public static void main(String args[]){
+        //output
+        System.out.println("Hello world with my college");
+        System.out.print("Hello world\n");
+        System.out.print("Hello world with java\nwith my college");
+        // Quiz
+    }
+}
