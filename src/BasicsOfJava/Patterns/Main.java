b2b0fb73c641dcd43ee9f34be80c392c74d4543c
metachemist <meta.chemist.22@gmail.com>
2024-05-06 19:37:02 +0500

    shifting main.java to pattern folder

---
+++ src/BasicsOfJava/Patterns/Main.java
@@ -0,0 +1,31 @@
+package BasicsOfJava.Patterns;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        //output
+        System.out.println("Hello world with my college");
+        System.out.print("Hello world\n");
+        System.out.print("Hello world with java\nwith my college\n");
+        // variables
+        String name = "tony stark";
+        int age = 48;
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+        //
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+
+    }
+}
+ 
