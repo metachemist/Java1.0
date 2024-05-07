d2dd9e36654b344ad85371086813f57b47d8a937
metachemist <meta.chemist.22@gmail.com>
2024-05-07 10:52:28 +0500

    Adding Inverted Half Pyramid Pattern

---
+++ src/BasicsOfJava/Patterns/invertedHalfPyramid.java
@@ -0,0 +1,12 @@
+package BasicsOfJava.Patterns;
+
+public class invertedHalfPyramid {
+    public static void main(String[] args){
+        for (int i=4; i>=1; i--){
+            for (int j=i; j>=1; j--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
