fbb72fbaaa507ab051275e540f932032227cc21a
metachemist <meta.chemist.22@gmail.com>
2024-05-10 13:10:49 +0500

    updating count the character

--- src/BasicsOfJava/w3resources/Part01/CountTheCharacters.java
+++ src/BasicsOfJava/w3resources/Part01/CountTheCharacters.java
@@ -1,25 +1,25 @@
 package BasicsOfJava.w3resources.Part01;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CountTheCharacters {
-    public static void main(String[] args){
-        System.out.println("Enter Any string: ");
+    public static void main(String[] args) {
+        System.out.println("Enter any string: ");
         Scanner input = new Scanner(System.in);
-        char[] test01 = input.nextLine().toCharArray();
-        count(Arrays.toString(test01));
-
+        String test01 = input.nextLine();
+        input.close();
+        count(test01);
     }
-    public static void count(String test01){
+
+    public static void count(String test01) {
         char[] ch = test01.toCharArray();
         int letter = 0;
         int space = 0;
         int num = 0;
         int other = 0;
 
-        for (int i = 0; i < test01.length(); i++){
-            if (Character.isLetter(ch[i])){
+        for (int i = 0; i < test01.length(); i++) {
+            if (Character.isLetter(ch[i])) {
                 letter++;
             } else if (Character.isDigit(ch[i])) {
                 num++;
@@ -29,10 +29,10 @@ public class CountTheCharacters {
                 other++;
             }
         }
-        System.out.println(STR."The String is : \{test01}");
-        System.out.println(STR."Letter: \{letter}");
-        System.out.println(STR."Space: \{space}");
-        System.out.println(STR."Number: \{num}");
-        System.out.println(STR."Other Characters: \{other}");
+        System.out.println("The string is: " + test01);
+        System.out.println("Letters: " + letter);
+        System.out.println("Spaces: " + space);
+        System.out.println("Numbers: " + num);
+        System.out.println("Other Characters: " + other);
     }
 }
