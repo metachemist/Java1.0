21450bba6c5ff77989d112178b8ade6d4272464b
metachemist <meta.chemist.22@gmail.com>
2024-05-10 12:58:22 +0500

    adding rotated half pyramid

--- src/BasicsOfJava/Patterns/rotatedHalfPyramid.java
+++ src/BasicsOfJava/Patterns/rotatedHalfPyramid.java
@@ -1,2 +1,28 @@
-package BasicsOfJava.Patterns;public class rotatedHalfPyramid {
+package BasicsOfJava.Patterns;
+
+public class rotatedHalfPyramid {
+    public static void main(String[] args){
+        int n=4;
+//        for (int i=1; i<=n; i++){
+//            // inner loop --> print space
+//            for (int j=1; j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            //inner loop --> print star
+//            for (int j=1; j<=i; j++){
+//                System.out.println("*");
+//            }
+//            System.out.println();
+//        }
+
+        for(int i=n; i>=1; i--) {
+            for(int j=1; j<i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<=n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
