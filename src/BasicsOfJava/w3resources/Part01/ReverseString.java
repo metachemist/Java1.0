7087a4e3cb3f97a92042ca5dc67c4b9cd41e49a2
metachemist <meta.chemist.22@gmail.com>
2024-05-10 13:29:10 +0500

    Updating reverse string

--- src/BasicsOfJava/w3resources/Part01/ReverseString.java
+++ src/BasicsOfJava/w3resources/Part01/ReverseString.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class ReverseString {
     public static void main(String[] args){
         Scanner input = new Scanner (System.in);
-        System.out.println(STR."Enter any string you want to reverse: ");
+        System.out.println("Enter any string you want to reverse: ");
         char[] str1 = input.nextLine().toCharArray();
-        System.out.println(STR."Reverse string: ");
+        System.out.println("Reverse string: ");
         for (int i = str1.length - 1; i >= 0; i--){
             System.out.print(str1[i]);
+     
         }
+        input.close();
     }
+
 }
