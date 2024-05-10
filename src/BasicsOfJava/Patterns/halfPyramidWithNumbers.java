37d91f111572145d1f1b5669fa908fb28486a1fe
metachemist <meta.chemist.22@gmail.com>
2024-05-10 11:00:26 +0500

    Adding half pyramid with numbers

---
+++ src/BasicsOfJava/Patterns/halfPyramidWithNumbers.java
@@ -0,0 +1,10 @@
+package BasicsOfJava.Patterns;
+
+public class halfPyramidWithNumbers {
+    public static void main(String[] args){
+        int n=4;
+        for (int i=1; i<=n; i++){
+            System.out.println("*");
+        }
+    }
+}
