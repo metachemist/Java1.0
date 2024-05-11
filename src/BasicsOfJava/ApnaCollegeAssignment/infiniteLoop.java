caf82a11f7ca4085b82f2625bf6bc58eadb235ad
metachemist <meta.chemist.22@gmail.com>
2024-05-11 09:53:53 +0500

    Apna college assignment question 06

---
+++ src/BasicsOfJava/ApnaCollegeAssignment/infiniteLoop.java
@@ -0,0 +1,13 @@
+package BasicsOfJava.ApnaCollegeAssignment;
+
+import java.util.Scanner;
+
+public class infiniteLoop {
+    public static void main(String[] args){
+        int i=0;
+        do{
+            System.out.println("hello hafsa, this is infinite loop");
+            i++;
+        } while (true);
+    }
+}
