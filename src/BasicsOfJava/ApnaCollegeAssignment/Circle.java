0fd7832cd8d78df400a37b7a023ecf845c3d7e7b
metachemist <meta.chemist.22@gmail.com>
2024-05-11 08:56:21 +0500

    Apna college assignment question 04

--- src/BasicsOfJava/ApnaCollegeAssignment/Circle.java
+++ src/BasicsOfJava/ApnaCollegeAssignment/Circle.java
@@ -1,14 +1,20 @@
 package BasicsOfJava.ApnaCollegeAssignment;
+import static java.lang.Math.PI;
 
 import java.util.Scanner;
 
 public class Circle {
 
     public static double circumferenceOfCircle(double radius){
-        
+        return ((2)*radius*PI);
     }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius of circle: ");
         double radius = input.nextInt();
+        double result = circumferenceOfCircle(radius);
+        System.out.println("The Circumference of the circle is: " + result);
+
+        input.close();
     }
 }
