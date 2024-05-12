93a9c74cdc0ed5deb4117add921a2318f4f8b5f2
metachemist <meta.chemist.22@gmail.com>
2024-05-12 18:18:14 +0500

    Apna College question 07

---
+++ src/BasicsOfJava/ApnaCollegeAssignment/countNumbers.java
@@ -0,0 +1,15 @@
+package BasicsOfJava.ApnaCollegeAssignment;
+
+import java.util.Scanner;
+
+public class countNumbers {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers you want to enter: ");
+        int n = input.nextInt();
+        int i=0;
+        while(i==n){
+            double 
+        }
+    }
+}
